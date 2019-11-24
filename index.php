7b50bd369f2187d16549a0c956c888df394c74bf
pulpnon-fiction <51931212+pulpnon-fiction@users.noreply.github.com>
2019-11-24 14:33:34 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,44 +9,57 @@ $loc = isset($_GET['branch']) ? $_GET['branch'] : null;
 $time = isset($_GET['fromdate'], $_GET['fromtime'], $_GET['todate'], $_GET['totime'])
              ? array($_GET['fromdate'], $_GET['fromtime'], $_GET['todate'], $_GET['totime']) : null;
 
-if($type){
+// only cartype
+if($type !== '0' && $loc == '0' && empty($_GET['fromdate']) && empty($_GET['todate'])){
     $template->vehicles = $vehicle->getVType($type);
-    $url = "vtname=".$type;
+    $url = "";
  }
 
-//  if($loc){
-//     $loc_arr = explode("#", $loc);
-//     $template->vehicles = $vehicle->getVTypesByLoc($loc_arr[0], $loc_arr[1]);
-//     $url = "location=".$loc_arr[0]."&city=".$loc_arr[1];
-//  }
+// only location
+ if($loc !== '0' && $type == '0' && empty($_GET['fromdate']) && empty($_GET['todate'])){
+    $loc_arr = explode("#", $loc);
+    $template->vehicles = $vehicle->getVTypesByLoc($loc_arr[0], $loc_arr[1]);
+    $url = "&location=".$loc_arr[0]."&city=".$loc_arr[1];
+ }
 
-//  if($time){
-//     $template->vehicles = $vehicle->getVTypesByTime($time[0], $time[1], $time[2], $time[3]);
-//     $url = "fromdate=".$time[0]."&fromtime=".$time[1]."&todate=".$time[2]."&totime=".$time[3];
-//  }
+// only time interval
+ if($loc == '0' && $type == '0' && !empty($_GET['fromdate']) && !empty($_GET['todate'])){
+    $template->vehicles = $vehicle->getVTypesByTime($time[0], $time[1], $time[2], $time[3]);
+    $url = "&fromdate=".$time[0]."&fromtime=".$time[1]."&todate=".$time[2]."&totime=".$time[3];
+ }
 
-//  if($type && $loc){
-//     $loc_arr = explode("#", $loc);
-//     $template->vehicles = $vehicle->getVTypeByTypeLoc($type, $loc_arr[0], $loc_arr[1]);
-//     $url = "vtname=".$type."&location=".$loc_arr[0]."&city=".$loc_arr[1];
-//  }
+// type and location
+ if($loc !== '0' && $type !== '0' && empty($_GET['fromdate']) && empty($_GET['todate'])){
+    $loc_arr = explode("#", $loc);
+    $template->vehicles = $vehicle->getVTypeByTypeLoc($type, $loc_arr[0], $loc_arr[1]);
+    $url = "&location=".$loc_arr[0]."&city=".$loc_arr[1];
+ }
 
-//  if($type && $time){
-//     $template->vehicles = $vehicle->getVTypeByTypeTime($type, $time[0], $time[1], $time[2], $time[3]);
-//     $url = "vtname=".$type."&fromdate=".$time[0]."&fromtime=".$time[1]."&todate=".$time[2]."&totime=".$time[3];
-//  }
+// type and time
+ if($type !== '0' && $loc == '0' && !empty($_GET['fromdate']) && !empty($_GET['todate'])){
+    $template->vehicles = $vehicle->getVTypeByTypeTime($type, $time[0], $time[1], $time[2], $time[3]);
+    $url = "&fromdate=".$time[0]."&fromtime=".$time[1]."&todate=".$time[2]."&totime=".$time[3];
+ }
 
-//  if($loc && $time){
-//     $loc_arr = explode("#", $loc);
-//     $template->vehicles = $vehicle->getVTypesLocTime($loc_arr[0], $loc_arr[1], $time[0], $time[1], $time[2], $time[3]);
-//     $url = "location=".$loc_arr[0]."&city=".$loc_arr[1]."&fromdate=".$time[0]."&fromtime=".$time[1]."&todate=".$time[2]."&totime=".$time[3];
-//  }
+// location and time
+ if($type == '0' && $loc !== '0' && !empty($_GET['fromdate']) && !empty($_GET['todate'])){
+    $loc_arr = explode("#", $loc);
+    $template->vehicles = $vehicle->getVTypesLocTime($loc_arr[0], $loc_arr[1], $time[0], $time[1], $time[2], $time[3]);
+    $url = "&location=".$loc_arr[0]."&city=".$loc_arr[1]."&fromdate=".$time[0]."&fromtime=".$time[1]."&todate=".$time[2]."&totime=".$time[3];
+ }
 
-//  if($type && $loc && $time){
-//     $loc_arr = explode("#", $loc);
-//     $template->vehicles = $vehicle->getVTypeByTypeLocTime($loc_arr[0], $loc_arr[1], $time[0], $time[1], $time[2], $time[3]);
-//     $url = "vtname=".$type."&location=".$loc_arr[0]."&city=".$loc_arr[1]."&fromdate=".$time[0]."&fromtime=".$time[1]."&todate=".$time[2]."&totime=".$time[3];
-//  }
+//all 3
+ if($type !== '0' && $loc !== '0' && !empty($_GET['fromdate']) && !empty($_GET['todate'])){
+    $loc_arr = explode("#", $loc);
+    $template->vehicles = $vehicle->getVTypeByTypeLocTime($type, $loc_arr[0], $loc_arr[1], $time[0], $time[1], $time[2], $time[3]);
+    $url = "&location=".$loc_arr[0]."&city=".$loc_arr[1]."&fromdate=".$time[0]."&fromtime=".$time[1]."&todate=".$time[2]."&totime=".$time[3];
+ }
+
+// none selected
+ if($type == '0' && $loc == '0' && empty($_GET['fromdate']) && empty($_GET['todate'])){
+    $template->vehicles = $vehicle->getAll();
+    $url = "";
+ }
 
 $template->types = $vehicle->getTypes();
 $template->branches = $vehicle->getBranches();
